06ddfa934e0c30c374643f917bdbb0bd59865556
ainame <s.namai.09@gmail.com>
2025-08-30 06:23:50 +0100

    Remove unimplemented commands

--- Sources/Command/Command.swift
+++ Sources/Command/Command.swift
@@ -8,47 +8,8 @@ struct MainCommand: AsyncParsableCommand {
         commandName: "tuzuru",
         subcommands: [
             InitCommand.self,
-            AddCommand.self,
-            PreviewCommand.self,
             GenerateCommand.self,
-            WatchCommand.self,
         ],
         defaultSubcommand: GenerateCommand.self,
     )
-}
-
-struct AddCommand: AsyncParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "add",
-    )
-
-    @Argument
-    var title: String
-
-    mutating func run() async throws {
-        fatalError("To be implemented")
-    }
-}
-
-struct PreviewCommand: AsyncParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "preview",
-    )
-
-    @Option(name: .shortAndLong)
-    var port: Int = 8080
-
-    mutating func run() async throws {
-        fatalError("To be implemented")
-    }
-}
-
-struct WatchCommand: AsyncParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "watch",
-    )
-
-    mutating func run() async throws {
-        fatalError("To be implemented")
-    }
 }
