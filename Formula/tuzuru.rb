747babb601fbb9dacbe5a54d7a50c7bce94c5038
GitHub Action <action@github.com>
2025-08-31 23:26:33 +0000

    Update Formula for version 0.0.6

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -2,7 +2,7 @@ class Tuzuru < Formula
   desc "Simple static blog generator"
   homepage "https://github.com/ainame/Tuzuru"
   url "https://github.com/ainame/Tuzuru/releases/download/0.0.6/tuzuru-0.0.6-macos-universal.tar.gz"
-  sha256 "1bbd3cec2b18f727fa35d4f4208814e026a119a0942382104e901362ea4cbe19"
+  sha256 "0b9ac3a169f868d65942ba9b1342faaec451c45ae39c14e0cbbda393ffc1c2c8"
   license "MIT"
   head "https://github.com/ainame/Tuzuru.git", branch: "main"
 
