3a177b348a4b88c0773e602dcb88f6e552d64cc8
ainame <s.namai.09@gmail.com>
2025-08-31 19:00:34 +0100

    Bump version to 0.0.4

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -1,7 +1,7 @@
 class Tuzuru < Formula
   desc "Static site generator for Swift developers"
   homepage "https://github.com/ainame/Tuzuru"
-  url "https://github.com/ainame/Tuzuru/releases/download/0.0.3/tuzuru-0.0.3-macos-universal.tar.gz"
+  url "https://github.com/ainame/Tuzuru/releases/download/0.0.4/tuzuru-0.0.4-macos-universal.tar.gz"
   sha256 "1dcd1978bdc95d5ab7ae6c29a128f75da4ab5bc7adc8b360ab27dbc463e53c40"
   license "MIT"
   head "https://github.com/ainame/Tuzuru.git", branch: "main"
--- Sources/Command/Command.swift
+++ Sources/Command/Command.swift
@@ -7,7 +7,7 @@ struct MainCommand: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "tuzuru",
         abstract: "Simple static blog generator",
-        version: "0.0.3",
+        version: "0.0.4",
         subcommands: [
             InitCommand.self,
             GenerateCommand.self,
