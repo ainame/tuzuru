7286c0fb37c7dc8ff57cb9314142a625469ccbe0
ainame <s.namai.09@gmail.com>
2025-09-01 00:03:39 +0100

    Fix formula description

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -1,5 +1,5 @@
 class Tuzuru < Formula
-  desc "Static site generator for Swift developers"
+  desc "Simple static blog generator"
   homepage "https://github.com/ainame/Tuzuru"
   url "https://github.com/ainame/Tuzuru/releases/download/0.0.5/tuzuru-0.0.5-macos-universal.tar.gz"
   sha256 "1bbd3cec2b18f727fa35d4f4208814e026a119a0942382104e901362ea4cbe19"
@@ -11,10 +11,10 @@ class Tuzuru < Formula
   def install
     # Install the binary in libexec instead of bin
     libexec.install "tuzuru"
-    
+
     # Install bundle resources in pkgshare
     pkgshare.install "tuzuru_TuzuruLib.bundle"
-    
+
     # Create wrapper script in bin that sets TUZURU_RESOURCES environment variable
     (bin/"tuzuru").write_env_script libexec/"tuzuru", TUZURU_RESOURCES: pkgshare
   end
