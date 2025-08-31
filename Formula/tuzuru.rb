7b827687aadfef3fb97c7a9e721b849d1a0dfd7e
GitHub Action <action@github.com>
2025-08-31 11:58:36 +0000

    Update Formula for version 0.0.2

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -2,7 +2,7 @@ class Tuzuru < Formula
   desc "Static site generator for Swift developers"
   homepage "https://github.com/ainame/Tuzuru"
   url "https://github.com/ainame/Tuzuru/releases/download/0.0.2/tuzuru-0.0.2-macos-universal.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "4089936c3fc4ce702eb9fb294b0d6ef665006ca5297c7cfeb73a23f0aced472f"
   license "MIT"
   head "https://github.com/ainame/Tuzuru.git", branch: "main"
 
