b339101c443ab5e29cd7320601d2941c30a97cd9
GitHub Action <action@github.com>
2025-09-03 01:44:42 +0000

    chore: update Formula for 0.0.14

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -2,7 +2,7 @@ class Tuzuru < Formula
   desc "Simple static blog generator"
   homepage "https://github.com/ainame/Tuzuru"
   url "https://github.com/ainame/Tuzuru/releases/download/0.0.14/tuzuru-0.0.14-macos-universal.tar.gz"
-  sha256 "4a13b703a5702251b07cbfad1e14c8aa8aaa4b48b04c5dbc8637eb374da3ca89"
+  sha256 "661c5ed83e8031222fdbf7b89a1dd3ffc69bb4581ebe9c9e6138a93fa4e8b93f"
   license "MIT"
   head "https://github.com/ainame/Tuzuru.git", branch: "main"
 
