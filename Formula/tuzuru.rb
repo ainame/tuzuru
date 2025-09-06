10e8d0173c57926bc486867d9addc1f25d4e364c
ainame <s.namai.09@gmail.com>
2025-09-06 20:54:20 +0100

    Bump version to 0.1.2

--- Formula/tuzuru.rb
+++ Formula/tuzuru.rb
@@ -1,7 +1,7 @@
 class Tuzuru < Formula
   desc "Simple static blog generator"
   homepage "https://github.com/ainame/Tuzuru"
-  url "https://github.com/ainame/Tuzuru/releases/download/0.1.1/tuzuru-0.1.1-macos-universal.tar.gz"
+  url "https://github.com/ainame/Tuzuru/releases/download/0.1.2/tuzuru-0.1.2-macos-universal.tar.gz"
   sha256 "21ce047cf30ea871c3dd44a7e4484ce86feb4737b9ca39daffc8365a8448d2a7"
   license "MIT"
   head "https://github.com/ainame/Tuzuru.git", branch: "main"
