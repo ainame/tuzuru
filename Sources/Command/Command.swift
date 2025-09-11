6bdc14e2420b5d4de9d52c796dd9257a805f27a3
ainame <s.namai.09@gmail.com>
2025-09-11 02:44:03 +0100

    Bump version to 0.2.0

--- Sources/Command/Command.swift
+++ Sources/Command/Command.swift
@@ -7,7 +7,7 @@ struct MainCommand: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "tuzuru",
         abstract: "Simple static blog generator",
-        version: "0.1.2",
+        version: "0.2.0",
         subcommands: [
             InitCommand.self,
             GenerateCommand.self,
